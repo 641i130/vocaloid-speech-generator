e20ca97432888250001c33fea80e8e6751d00b49
TheEnchantedCow <galileoray@gmail.com>
2019-10-22 16:07:25 -0700

    Update

--- create-save.py
+++ create-save.py
@@ -4,6 +4,12 @@ import binascii
 import json
 import codecs
 
+# First you need to generate a json file. 
+# Then put it into a folder named Project.
+# Next you would add the contents to it that generates the speech.
+# Lastly you would compress the file and name the zip file as a .vpr file. This process is for Vocaloid 5
+
+
 file = "test.vpr"
 
 f = codecs.open(file, 'rb', '')
